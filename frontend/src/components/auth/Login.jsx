a21d325c38060615e32de9a5ac74b7c39b7e2b52
Nikhil Shinde <parvatisaud774@gmail.com>
2025-08-11 17:18:55 +0530

    auth

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -66,7 +66,7 @@ const Login = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="parvati@gmail.com"
                         />
                     </div>
 
@@ -77,7 +77,7 @@ const Login = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="parvati@2004"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
