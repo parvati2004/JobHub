04d33d5c0c78a1a8d3c6e06c3fe190112adeeb89
Nikhil Shinde <parvatisaud774@gmail.com>
2025-08-11 17:55:01 +0530

    cmpsetup

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -45,10 +45,12 @@ const CompanySetup = () => {
             formData.append("file", input.file);
         }
         try {
-            setLoading(true);
+           // setLoading(true);
+             const token = localStorage.getItem("token");
             const res = await axios.put(`${COMPANY_API_END_POINT}/update/${params.id}`, formData, {
                 headers: {
-                    'Content-Type': 'multipart/form-data'
+                   // 'Content-Type': 'multipart/form-data'
+                      Authorization: `Bearer ${token}`
                 },
                 withCredentials: true
             });
